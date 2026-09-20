fd0609fda1fa16385879fd3a8ed6ad807d916c08
Blizz127 <blizz127@gmail.com>
2026-09-20 07:35:26 -0500

    leaf func_80090AAC: two-call wrapper with s0/s1 frame; VRAM 0x80090AAC file 0x812AC size 0x40; era_o2_g0_aspsx_230

---
+++ src/func_80090AAC.c
@@ -0,0 +1,8 @@
+/* VRAM 0x80090AAC / file 0x812AC / size 0x40. */
+extern void func_8009059C(void);
+extern void func_800905C4(int arg0, int arg1);
+
+void func_80090AAC(int arg0, int arg1) {
+    func_8009059C();
+    func_800905C4(arg0, arg1);
+}
